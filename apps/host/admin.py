3c36df509b50966c3ffa24fb5f2cc940af2a08a2
三石 <sanshi@zmops.com>
2022-03-31 14:33:01 +0800

    增加admin功能

--- apps/host/admin.py
+++ apps/host/admin.py
@@ -14,12 +14,13 @@ from zabbix.api import ApiHost
 @admin.register(Switch)
 class SwitchAdmin(admin.ModelAdmin):
 
-    list_display = ('code', 'name', 'station', 'model_id', 'status_operation', 'create_time', )
+    list_display = ('code', 'name', 'station', 'template_id', 'status_operation', 'create_time', 'operation', )
     ordering = ('-create_time', )
     list_per_page = 20
-    list_filter = ['model_id', ]
-    search_fields = ('name', )
-    exclude = ['host', 'host_id', 'status', ]
+    list_filter = ['station__name', 'status', ]
+    search_fields = ('name', 'code', )
+    readonly_fields = ('code', )
+    exclude = ['zabbix_code', 'zabbix_id', 'status', ]
 
     def response_add(self, request, obj, post_url_continue=None):
         if messages.get_messages(request):
@@ -36,12 +37,12 @@ class SwitchAdmin(admin.ModelAdmin):
         api = ApiHost()
 
         # 创建
-        if not obj.host_id:
+        if not obj.zabbix_id:
             response = api.create_snmp(
                 name=obj.name,
-                host=str(obj.host),
-                group_id=obj.station.host_group_id,
-                model_id=obj.model_id,
+                host=str(obj.zabbix_code),
+                group_id=obj.station.zabbix_id,
+                template_id=obj.template_id,
                 ipaddress=obj.ipaddress,
                 port=obj.port,
                 snmp_community=obj.snmp_community,
@@ -49,10 +50,10 @@ class SwitchAdmin(admin.ModelAdmin):
         # 更新
         else:
             response = api.update_snmp(
-                host_id=obj.host_id,
+                host_id=obj.zabbix_id,
                 name=obj.name,
-                group_id=obj.station.host_group_id,
-                model_id=obj.model_id,
+                group_id=obj.station.zabbix_id,
+                template_id=obj.template_id,
                 ipaddress=obj.ipaddress,
                 port=obj.port,
                 snmp_community=obj.snmp_community,
@@ -65,20 +66,23 @@ class SwitchAdmin(admin.ModelAdmin):
             messages.warning(request, EnumMsg.ZabbixAPIError.value.format(api.error))
             return
         else:
-            obj.host_id = host_id
+            obj.zabbix_id = host_id
+            obj.save()
+
+            obj.code = Switch.Prefix.format(obj.id)
             obj.save()
 
     def delete_model(self, request, obj):
 
         api = ApiHost()
-        api.delete(host_ids=[obj.host_id])
+        api.delete(host_ids=[obj.zabbix_id])
         obj.delete()
 
     def delete_queryset(self, request, queryset):
 
         host_ids = []
         for obj in queryset:
-            host_ids.append(obj.host_id)
+            host_ids.append(obj.zabbix_id)
 
         api = ApiHost()
         api.delete(host_ids=host_ids)
