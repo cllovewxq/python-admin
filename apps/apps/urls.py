7cf48e3fafe71548fd309a218949ae8b0f99ef14
三石 <sanshi@zmops.com>
2022-03-31 14:31:23 +0800

    url提交

--- apps/apps/urls.py
+++ apps/apps/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from base import views as base_views
 from base.views.favicon import favicon_view
+from history import views as history_views
 from host import views as host_views
-from zabbix.views.test import TestAPIView
+from problem import views as problem_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,9 +30,13 @@ urlpatterns = [
     path('host/switch/enable/<switch_id>/', host_views.SwitchEnableAPIView.as_view(), name="交换机启用"),
     path('host/switch/disable/<switch_id>/', host_views.SwitchDisableAPIView.as_view(), name="交换机禁用"),
 
-    path('base/', base_views.BaseTemplateView.as_view(), name="基础"),
+    path('problem/problem/acknowledge/<problem_id>/', problem_views.ProblemAcknowledgeAPIView.as_view(), name="告警确认"),
 
-    path('test/', TestAPIView.as_view(), name="测试"),
+    path('history/<host_id>/get/', history_views.HistoryHTMLView.as_view(), name="历史"),
+    path('api/problem/create/', problem_views.ProblemCreateAPIView.as_view(), name="告警创建"),
+    path('api/problem/solve/', problem_views.ProblemSolveAPIView.as_view(), name="告警解决"),
+
+    path('base/', base_views.BaseTemplateView.as_view(), name="基础"),
 ]
 
 
